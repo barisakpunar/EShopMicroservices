a3515111fb708121d73e66ab876202538662de71
ES\barıs.akpunar <baris.akpunar@sipay.com.tr>
2025-12-08 17:25:54 +0300

    update

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        string Description,
+        List<string> Category,
+        decimal Price,
+        string ImageFile) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Description = request.Description,
+                Category = request.Category,
+                Price = request.Price,
+                ImageFile = request.ImageFile
+            };  
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
+
+        }
+    }
+}
